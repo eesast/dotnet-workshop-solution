01e4b4903386b505cd5fc17376aaf7291e9b58d4
Timothy Liu <liuxf19@163.com>
2026-06-17 03:59:23 +0800

    chore: :wrench: init dotnet environment

---
+++ src/test-00-prepare/TestPrepare.cs
@@ -0,0 +1,13 @@
+﻿namespace test_00_prepare
+{
+    [TestClass]
+    public sealed class Test00Prepare
+    {
+        [TestMethod]
+        public void Test00_Welcome()
+        {
+            bool isEnvironmentReady = true;
+            Assert.IsTrue(isEnvironmentReady, "Welcome to your dotnet-workshop journey!");
+        }
+    }
+}
