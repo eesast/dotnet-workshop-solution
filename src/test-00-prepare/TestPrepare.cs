8a26b99f2ca9f531647bfc9a599f388aa6d3da8b
Timothy Liu <liuxf19@163.com>
2026-06-27 18:31:30 +0800

    test(prepare): :white_check_mark: rename TestWelcome

--- src/test-00-prepare/TestPrepare.cs
+++ src/test-00-prepare/TestPrepare.cs
@@ -3,8 +3,8 @@
     [TestClass]
     public sealed class Test00Prepare
     {
-        [TestMethod]
-        public void Test00_Welcome()
+        [TestMethod(DisplayName = "T0.1.1 TestWelcome")]
+        public void TestWelcome()
         {
             bool isEnvironmentReady = true;
             Assert.IsTrue(isEnvironmentReady, "Welcome to your dotnet-workshop journey!");
